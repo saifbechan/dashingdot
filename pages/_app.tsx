e008d3366b60d3184c0b97a783ad3819805b3963
Saif Bechan <saifbechan@gmail.com>
2021-02-14 16:18:30 +0100

    opened repo for the public

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -12,7 +12,7 @@ export default function MyApp({ Component, pageProps }: AppProps): JSX.Element {
         <meta name="theme-color" content="#141526" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" type="image/x-icon" sizes="64x64 32x32 24x24 16x16" />
-        <link rel="apple-touch-icon" href="/android-chrome-192x192.png" />
+        <link rel="apple-touch-icon" href="/favicon.png" />
         <link rel="manifest" href="/manifest.json" />
         <meta name="keywords" content="react, typescript, javascript, github, nextjs" />
       </Head>
